a2ca38ad379514c00c2b1e8f9d38021735892489
Bertrand Roussel <broussel@sierrawireless.com>
2016-03-14 16:15:52 -0700

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -8,21 +8,37 @@
 # RUN /usr/local/bin/plugins.sh /plugins.txt
 #
 
-set -e
-
 REF=/usr/share/jenkins/ref/plugins
 mkdir -p $REF
+
+if [ -z "$JENKINS_UC_DOWNLOAD" ]; then
+    JENKINS_UC_DOWNLOAD=$JENKINS_UC/download
+fi
+
+get_plugin() {
+    curl -L ${JENKINS_UC_DOWNLOAD}/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.jpi;
+    echo "=> $(ls -l $REF/${plugin[0]}.jpi)"
+    if file $REF/${plugin[0]}.jpi | grep -v Zip; then
+        echo "Failed to get jpi"
+        return 1
+    fi
+    unzip -qqt $REF/${plugin[0]}.jpi
+    return 0
+}
 
 while read spec || [ -n "$spec" ]; do
     plugin=(${spec//:/ });
     [[ ${plugin[0]} =~ ^# ]] && continue
     [[ ${plugin[0]} =~ ^\s*$ ]] && continue
     [[ -z ${plugin[1]} ]] && plugin[1]="latest"
-    echo "Downloading ${plugin[0]}:${plugin[1]}"
-
-    if [ -z "$JENKINS_UC_DOWNLOAD" ]; then
-      JENKINS_UC_DOWNLOAD=$JENKINS_UC/download
+    plugin=(${spec//:/ })
+    echo "Downloading ${plugin[0]} : ${plugin[1]}"
+    if ! get_plugin; then
+        echo "Failed, retrying"
+        if ! get_plugin; then
+            echo "Failed again, exiting"
+            exit 1
+        fi
     fi
-    curl -sSL -f ${JENKINS_UC_DOWNLOAD}/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.jpi
-    unzip -qqt $REF/${plugin[0]}.jpi
 done  < $1
+
